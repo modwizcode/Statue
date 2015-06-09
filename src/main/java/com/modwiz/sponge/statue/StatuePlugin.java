a367fe46d193c93f27c049abac7142301e48f05d
Jamie Mansfield <malgm@lapis.blue>
2015-06-09 22:31:12 +0100

    Use license plugin

--- src/main/java/com/modwiz/sponge/statue/StatuePlugin.java
+++ src/main/java/com/modwiz/sponge/statue/StatuePlugin.java
@@ -1,3 +1,26 @@
+/*
+ * This file is part of Statues, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2015, Starbuck Johnson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.modwiz.sponge.statue;
 
 import com.google.inject.Inject;
@@ -38,7 +61,7 @@ public class StatuePlugin {
             game.getServiceManager().setProvider(this, CacheForwardingService.class, profileResolver);
             logger.info("Registered profile resolver service successfully.");
         } catch (ProviderExistsException e) {
-            logger.error("An error occured while setting the profile resolver service.", e);
+            logger.error("An error occurred while setting the profile resolver service.", e);
         }
 
         logger.info("Registering skin resolver service.");
@@ -47,7 +70,7 @@ public class StatuePlugin {
             game.getServiceManager().setProvider(this, SkinResolverService.class, skinResolver);
             logger.info("Registered skin resolver service successfully.");
         } catch (ProviderExistsException e) {
-            logger.error("An error occured while setting the skin resolver service.", e);
+            logger.error("An error occurred while setting the skin resolver service.", e);
         }
 
         CommandSpec testResolve = CommandSpec.builder()
